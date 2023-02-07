285aab72f85296f3e14e9a0885350296efcc52df
rnrnshn <olymangue@gmail.com>
2023-02-07 13:04:51 +0200

    add some sections

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,85 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.container}>
+        <section className={styles.features}>
+          <aside className="featureCards">
+            <div className="card">
+              <h3>É profissional de tecnologia?</h3>
+              <p>
+                Com uma comunidade que ostenta uma interessante
+                mistura de profissionais, sem duvidas o melhor
+                sitio para fazer networking.
+              </p>
+            </div>
+            <div className="card">
+              <h3>Pretende mudar de carreira para a tecnologia?</h3>
+              <p>
+                Pretende mudar de carreira para tech? Parece assustador pois?
+                Fazer parte da comunidade lhe possibilita mergulhar suavemente
+                na sua carreira escolhida antes de dar o grande salto.
+              </p>
+            </div>
+            <div className="card">
+              <h3>É estudante de STEM?</h3>
+              <p>
+                Temos constantemente veteranos da indústria
+                e líderes que passam por aqui para ter conversas
+                e discussões num ambiente informal e isto revela-se
+                sempre um óptimo local para encontrar mentores.
+              </p>
+            </div>
+          </aside>
+          <aside className="featureCTA">
+            <h2>Aprendemos em comunidade</h2>
+            <p>
+              Proporcionamos oportunidades de partilha de conhecimento e desenvolvimento de habilidades para a criação de produtos digitais a jovens durante o period de formação e contribuímos para o desenvolvimento da sociedade atraves das soluções desenvolvidas.
+            </p>
+          </aside>
+        </section>
 
+        {/* Comexão e Integração */}
+        <section className={styles.programs}>
+          <div className={styles.programHeader}></div>
+          <div className={styles.programItems}>
+            <aside className="programItem">
+              <h3>Talentos</h3>
+              <p>
+                Capacitados e desenvolvemos talentos com o objective de preprarar e integrar no mercado de trabalho atraves da nossa rede de parceiros
+              </p>
+            </aside>
+            <aside className="programItem">
+              <h3>Startup</h3>
+              <p>
+                Apoiamos equipas multidisplinares com orientação e capacitação no desenvolvimento de soluções tecnológicas em ambientes de extrema incerteza.
+              </p>
+            </aside>
+            <aside className="programItem">
+              <h3>Corporate</h3>
+              <p>
+                Firmamos parceirias e trabalhamos com empresas e ONGs no desenvolvimento de soluções de base tecnológica para a resolução de problemas sociais.
+              </p>
+            </aside>
+          </div>
+        </section>
+
+        {/* Team */}
+        <section className={styles.teams}>
+          <div className={styles.teamsHeader}></div>
+          <div className={styles.teamsMembers}>
+            <div className="teamMember">
+              <h5>Igor L. Sambo</h5>
+              <p>Presidente</p>
+            </div>
+            <div className="teamMember">
+              <h5>Carrima Januario</h5>
+              <p>Gestora de Projectos</p>
+            </div>
+            <div className="teamMember">
+              <h5>Sabera Pedro</h5>
+              <p>Vice-Presidente</p>
+            </div>
+          </div>
+        </section>
       </div>
       <Footer />
     </div>
