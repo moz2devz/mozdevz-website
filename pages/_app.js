01afee701e83028f9741d7001ebbda46ac05615f
rnrnshn <olymangue@gmail.com>
2023-02-08 01:37:45 +0200

    change font from Inter to Hind

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
-import { Inter } from '@next/font/google'
+import { Hind } from '@next/font/google'
 
 // If loading a variable font, you don't need to specify the font weight
-const inter = Inter({ subsets: ['latin'] })
+const hindi = Hind({
+  weight: ['400', '600', '700'],
+  subsets: ['latin'],
+})
 
 function MyApp({ Component, pageProps }) {
   return (
-    <main className={inter.className}>
+    <main className={hindi.className}>
       <Component {...pageProps} />
     </main>
   )
