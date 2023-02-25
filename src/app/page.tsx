caa2b933a4713f6628edb847d673e9801a759778
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-02-25 21:59:54 +0200

    feat: update href of the first 'saiba mais' button to point to the about page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,7 +66,7 @@ export default function Home() {
               </p>
               <a
                 className="text-primary font-bold border-solid border border-red-500 py-2 px-4 inline-block max-w-max"
-                href="#"
+                href="/about"
               >
                 Saiba mais
               </a>
