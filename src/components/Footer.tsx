d7ee76c0d555682a7a93bb3a6b0e42d1fc7a0b87
SheltonFr <sheltonfjs@gmail.com>
2023-11-04 13:33:16 +0200

    feat: set links to media icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,12 +17,32 @@ const Footer: React.FC = () => {
       <Link href="/" className="text-2xl font-bold block">
         <Image src={logo} width={230} alt={''} />
       </Link>
-      <div className="flex gap-4 justify-center items-center">
-        <FacebookLogo color="#fff" size={32} />
-        <TwitterLogo size={32} color="#fff" />
-        <InstagramLogo size={32} color="#fff" />
-        <LinkedinLogo size={32} color="#fff" />
-        <YoutubeLogo size={32} color="#fff" />
+      <div className="flex gap-4 justify-center items-center transition-all duration-700">
+        <Link href={'https://www.facebook.com/mozdevz'} target="_blank">
+          <FacebookLogo color="#fff" size={32} />
+        </Link>
+
+        <Link
+          href={
+            'https://twitter.com/i/flow/login?redirect_after_login=%2Fmozdevz'
+          }
+          target="_blank"
+        >
+          <TwitterLogo size={32} color="#fff" />
+        </Link>
+
+        <Link href={'https://www.instagram.com/mozdevz/'} target="_blank">
+          <InstagramLogo size={32} color="#fff" />
+        </Link>
+        <Link
+          href={'https://www.linkedin.com/company/mozdevz/'}
+          target="_blank"
+        >
+          <LinkedinLogo size={32} color="#fff" />
+        </Link>
+        <Link href={'https://www.youtube.com/@mozdevz6592'} target="_blank">
+          <YoutubeLogo size={32} color="#fff" />
+        </Link>
       </div>
       <p className="text-slate-400 text-base leading-relaxed text-center">
         ©2023 Mozdevz Community. <br />
